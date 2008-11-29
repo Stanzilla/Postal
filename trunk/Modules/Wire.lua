05534212109b05d0814f1bc6e455d7d90ed433a7
xinhuan <xinhuan@6b6a7bc4-fa74-4755-881c-4601391aff25>
2008-11-29 11:13:30 +0000

    Add a workaround for ruRU locale for Wire module.

--- trunk/Modules/Wire.lua
+++ trunk/Modules/Wire.lua
@@ -27,6 +27,15 @@ local g, s, c
 g = "^%["..GOLD_AMOUNT.." "..SILVER_AMOUNT.." "..COPPER_AMOUNT.."%]$"
 s = "^%["..SILVER_AMOUNT.." "..COPPER_AMOUNT.."%]$"
 c = "^%["..COPPER_AMOUNT.."%]$"
+if GetLocale() == "ruRU" then
+	--Because ruRU is silly and has these constants.
+	--COPPER_AMOUNT = "%d |4медная монета:медные монеты:медных монет;"; -- Lowest value coin denomination
+	--SILVER_AMOUNT = "%d |4серебряная:серебряные:серебряных;"; -- Mid value coin denomination
+	--GOLD_AMOUNT = "%d |4золотая:золотые:золотых;"; -- Highest value coin denomination
+	g = "^%[%d+з %d+с %d+м%]$"
+	s = "^%[%d+с %d+м%]$"
+	c = "^%[%d+м%]$"
+end
 g = gsub(g, "%%d", "%%d+")
 s = gsub(s, "%%d", "%%d+")
 c = gsub(c, "%%d", "%%d+")
@@ -39,9 +48,28 @@ function Postal_Wire:onValueChangedFunc()
 	then
 		local copper = MoneyInputFrame_GetCopper(SendMailMoney)
 		if copper and copper > 0 then
-			SendMailSubjectEditBox:SetText("["..self.core:GetMoneyString(copper).."]")
+			if GetLocale() == "ruRU" then
+				self:ruRUonValueChangedFunc(copper)
+			else
+				SendMailSubjectEditBox:SetText("["..self.core:GetMoneyString(copper).."]")
+			end
 		else
 			SendMailSubjectEditBox:SetText("")
+		end
+	end
+end
+
+if GetLocale == "ruRU" then
+	function Postal_Wire:ruRUonValueChangedFunc(money)
+		local gold = floor(money / 10000)
+		local silver = floor((money - gold * 10000) / 100)
+		local copper = mod(money, 100)
+		if gold > 0 then
+			SendMailSubjectEditBox:SetText(format("[%d+з %d+с %d+м]", gold, silver, copper))
+		elseif silver > 0 then
+			SendMailSubjectEditBox:SetText(format("[%d+с %d+м]", silver, copper))
+		else
+			SendMailSubjectEditBox:SetText(format("[%d+м]", copper))
 		end
 	end
 end
